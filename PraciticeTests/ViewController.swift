583d58e9a6787c3855228abc8d0c43842b4fa3ae
sauna1137 <subscription.search@gmail.com>
2021-12-29 17:39:21 +0900

    テスト用のクラス、メソッドを追加

--- PraciticeTests/ViewController.swift
+++ PraciticeTests/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var subClass: SubClass!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.subClass = SubClass(viewController: self)
     }
 
+    class SubClass {
+        private let viewController: ViewController
+
+        init(viewController: ViewController) {
+            self.viewController = viewController
+        }
+
+        func multiply(num1: Int, num2: Int) -> Int {
+            return num1 * num2
+        }
+    }
 
 }
 
